ca99c8904f0627cdd8302c1d289d707b41d1b09c
Nikolay Zhivotenko <niko.zvt@gmail.com>
2022-05-19 12:45:32 +0300

    Changed names

--- Ariadne.Kernel/Ariadne.Kernel/Classes/Math/Polygon.cs
+++ Ariadne.Kernel/Ariadne.Kernel/Classes/Math/Polygon.cs
@@ -1,45 +1,45 @@
 ﻿namespace Ariadne.Kernel.Math
 {
     /// <summary>
-    /// Abstract class for hiding the implementation of the Polygon class
+    /// Abstract class for hiding the implementation of the Plane class
     /// </summary>
-    abstract class Polygon
+    abstract class Plane
     {
         /// <summary>
-        /// Polygon specific data
+        /// Plane specific data
         /// </summary>
-        protected MathNet.Spatial.Euclidean.Plane _polygon;
+        protected MathNet.Spatial.Euclidean.Plane _plane;
 
         /// <summary>
-        /// Virtual method to return the type of the polygon
+        /// Virtual method to return the type of the plane
         /// </summary>
-        /// <returns>Type of polygon</returns>
-        public abstract PolygonType GetPolygonType();
+        /// <returns>Type of plane</returns>
+        public abstract PlaneType GetPlaneType();
     }
 
     /// <summary>
-    /// Enumeration of all available types of polygons
+    /// Enumeration of all available types of planes
     /// </summary>
-    enum PolygonType
+    enum PlaneType
     {
-        Polygon3D,
+        Plane3D,
     }
 
     /// <summary>
-    /// Class of a polygon in three-dimensional space
+    /// Class of a plane in three-dimensional space
     /// </summary>
-    class Polygon3D : Polygon
+    class Plane3D : Plane
     {
         /// <summary>
-        /// Default constructor. Creates a zero polygon
+        /// Default constructor. Creates a zero plane
         /// </summary>
-        private Polygon3D()
+        private Plane3D()
         {
             var p1 = new MathNet.Spatial.Euclidean.Point3D(0.0f, 0.0f, 0.0f);
             var p2 = new MathNet.Spatial.Euclidean.Point3D(0.0f, 0.0f, 0.0f);
             var p3 = new MathNet.Spatial.Euclidean.Point3D(0.0f, 0.0f, 0.0f);
 
-            _polygon = MathNet.Spatial.Euclidean.Plane.FromPoints(p1, p2, p3);
+            _plane = MathNet.Spatial.Euclidean.Plane.FromPoints(p1, p2, p3);
         }
 
         /// <summary>
@@ -48,12 +48,12 @@
         /// <param name="p1">First point</param>
         /// <param name="p2">Second point</param>
         /// <param name="p3">Third point</param>
-        public Polygon3D(Vector3D p1, Vector3D p2, Vector3D p3)
+        public Plane3D(Vector3D p1, Vector3D p2, Vector3D p3)
         {
             var point1 = new MathNet.Spatial.Euclidean.Point3D(p1.X, p1.Y, p1.Z);
             var point2 = new MathNet.Spatial.Euclidean.Point3D(p2.X, p2.Y, p2.Z);
             var point3 = new MathNet.Spatial.Euclidean.Point3D(p3.X, p3.Y, p3.Z);
-            _polygon = MathNet.Spatial.Euclidean.Plane.FromPoints(point1, point2, point3);
+            _plane = MathNet.Spatial.Euclidean.Plane.FromPoints(point1, point2, point3);
         }
 
         /// <summary>
@@ -64,25 +64,25 @@
         public float SignedDistanceTo(Vector3D point)
         {
             var p = new MathNet.Spatial.Euclidean.Point3D(point.X, point.Y, point.Z);
-            return (float)_polygon.SignedDistanceTo(p);
+            return (float)_plane.SignedDistanceTo(p);
         }
 
         /// <summary>
-        /// Returns the polygon type
+        /// Returns the plane type
         /// </summary>
-        /// <returns>Polygon type</returns>
-        public override PolygonType GetPolygonType()
+        /// <returns>Plane type</returns>
+        public override PlaneType GetPlaneType()
         {
-            return PolygonType.Polygon3D;
+            return PlaneType.Plane3D;
         }
 
         /// <summary>
-        /// The method returns representations of the specific polygon as a string
+        /// The method returns representations of the specific plane as a string
         /// </summary>
         /// <returns>A string that represents the current object</returns>
         public override string ToString()
         {
-            return GetPolygonType().ToString();
+            return GetPlaneType().ToString();
         }
     }
  }
