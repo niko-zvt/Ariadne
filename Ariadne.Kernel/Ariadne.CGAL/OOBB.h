105221ee63aa15c6674eefc102974838bce08b44
Nikolay Zhivotenko <niko.zvt@gmail.com>
2022-05-26 21:24:30 +0300

    Added OOBB functionality

--- Ariadne.Kernel/Ariadne.CGAL/OOBB.h
+++ Ariadne.Kernel/Ariadne.CGAL/OOBB.h
@@ -4,6 +4,7 @@
 // that uses this DLL. This way any other project whose source files include this file see 
 // CGAL_OOBB_API functions as being imported from a DLL, whereas this DLL sees symbols
 // defined with this macro as being exported.
+#pragma once
 #ifdef CGAL_OOBB_EXPORTS
 #define CGAL_OOBB_API __declspec(dllexport)
 #else
@@ -19,11 +20,14 @@
 #include <CGAL/Polyhedron_3.h>
 #include <CGAL/convex_hull_3.h>
 #include <CGAL/optimal_bounding_box.h>
+#include <CGAL/Delaunay_triangulation_3.h>
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel    Kernel;
 typedef Kernel::Point_3                                        Point3D;
 typedef CGAL::Polyhedron_3<Kernel>                             Polyhedron3D;
 typedef CGAL::Surface_mesh<Point3D>                            Surface_mesh;
+typedef CGAL::Delaunay_triangulation_3<Kernel>                 Delaunay;
+typedef Delaunay::Vertex_handle                                Vertex_handle;
 
 typedef void(__stdcall* Notification)(const char*);
 
@@ -34,7 +38,5 @@ typedef struct _AriadnePoint3D
     float y;
     float z;
 } AriadnePoint3D;
-
-std::string GetJSONByPointVector(const std::vector<Point3D>& points);
 
 extern "C" int32_t CGAL_OOBB_API __stdcall GetOptimalOrientedBoundingBox(AriadnePoint3D * points, int size, Notification notification);
