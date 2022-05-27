ee87edd07e215e765f3cfeae5c5b366ebdffb550
Nikolay Zhivotenko <niko.zvt@gmail.com>
2022-05-27 15:11:31 +0300

    Added error handling

--- Ariadne.Kernel/Ariadne.CGAL/AABB.cpp
+++ Ariadne.Kernel/Ariadne.CGAL/AABB.cpp
@@ -5,41 +5,48 @@
 // Manual test of FLOAT marshalling
 int32_t __stdcall GetAxisAlignedBoundingBox(AriadnePoint3D* points, int size, Notification notification)
 {
-    std::vector<Point3D> element_points;
-    for (int32_t i = 0; i < size; i++)
+    try
     {
-        auto x = points[i].x;
-        auto y = points[i].y;
-        auto z = points[i].z;
-        auto point = Point3D(x, y, z);
-        element_points.push_back(point);
-    }
+        std::vector<Point3D> element_points;
+        for (int32_t i = 0; i < size; i++)
+        {
+            auto x = points[i].x;
+            auto y = points[i].y;
+            auto z = points[i].z;
+            auto point = Point3D(x, y, z);
+            element_points.push_back(point);
+        }
 
-    // axis-aligned bounding box of 3D points
-    Kernel::Iso_cuboid_3 c3 = CGAL::bounding_box(element_points.begin(), element_points.end());
-    double xMin = c3.bbox().xmin();
-    double yMin = c3.bbox().ymin();
-    double zMin = c3.bbox().zmin();
-    double minLength = std::sqrt(xMin * xMin + yMin * yMin + zMin * zMin);
+        // axis-aligned bounding box of 3D points
+        Kernel::Iso_cuboid_3 c3 = CGAL::bounding_box(element_points.begin(), element_points.end());
+        double xMin = c3.bbox().xmin();
+        double yMin = c3.bbox().ymin();
+        double zMin = c3.bbox().zmin();
+        double minLength = std::sqrt(xMin * xMin + yMin * yMin + zMin * zMin);
 
-    double xMax = c3.bbox().xmax();
-    double yMax = c3.bbox().ymax();
-    double zMax = c3.bbox().zmax();
-    double maxLength = std::sqrt(xMax * xMax + yMax * yMax + zMax * zMax);
+        double xMax = c3.bbox().xmax();
+        double yMax = c3.bbox().ymax();
+        double zMax = c3.bbox().zmax();
+        double maxLength = std::sqrt(xMax * xMax + yMax * yMax + zMax * zMax);
 
-    std::string str = "";
-        
-    str +=  "{\"Length\":" + std::to_string(minLength) +
+        std::string str = "";
+
+        str += "{\"Length\":" + std::to_string(minLength) +
             ",\"X\":" + std::to_string(xMin) +
-            ",\"Y\":" + std::to_string(yMin) + 
+            ",\"Y\":" + std::to_string(yMin) +
             ",\"Z\":" + std::to_string(zMin) + "}\n";
 
-    str +=  "{\"Length\":" + std::to_string(maxLength) +
+        str += "{\"Length\":" + std::to_string(maxLength) +
             ",\"X\":" + std::to_string(xMax) +
             ",\"Y\":" + std::to_string(yMax) +
             ",\"Z\":" + std::to_string(zMax) + "}";
 
 
-    notification(str.c_str());
-    return 0;
+        notification(str.c_str());
+        return 0;
+    }
+    catch (const std::exception& ex)
+    {
+        auto wt = ex.what();
+    }
 }
--- Ariadne.Kernel/Ariadne.CGAL/OOBB.cpp
+++ Ariadne.Kernel/Ariadne.CGAL/OOBB.cpp
@@ -5,40 +5,47 @@
 // Manual test of FLOAT marshalling
 int32_t __stdcall GetOptimalOrientedBoundingBox(AriadnePoint3D* points, int size, Notification notification)
 {
-    std::vector<Point3D> element_points;
-    for (int32_t i = 0; i < size; i++)
+    try
     {
-        auto x = points[i].x;
-        auto y = points[i].y;
-        auto z = points[i].z;
-        auto point = Point3D(x, y, z);
-        element_points.push_back(point);
+        std::vector<Point3D> element_points;
+        for (int32_t i = 0; i < size; i++)
+        {
+            auto x = points[i].x;
+            auto y = points[i].y;
+            auto z = points[i].z;
+            auto point = Point3D(x, y, z);
+            element_points.push_back(point);
+        }
+
+        // Compute convex hull of element
+        Surface_mesh element_mesh;
+        CGAL::convex_hull_3(element_points.begin(), element_points.end(), element_mesh);
+        if (element_mesh.is_empty() && !element_mesh.is_valid())
+            return 1;
+        // Compute the extreme points of the mesh, and then a tightly fitted oriented bounding box
+        const int32_t boxSize = 8;
+        std::array<Point3D, boxSize> obb_points;
+        CGAL::oriented_bounding_box(element_mesh, obb_points, CGAL::parameters::use_convex_hull(true));
+
+        std::string str = "";
+
+        for (int32_t i = 0; i < boxSize; i++)
+        {
+            auto x = obb_points[i].x();
+            auto y = obb_points[i].y();
+            auto z = obb_points[i].z();
+            auto length = std::sqrt(x * x + y * y + z * z);
+            str += "{\"Length\":" + std::to_string(length) +
+                ",\"X\":" + std::to_string(x) +
+                ",\"Y\":" + std::to_string(y) +
+                ",\"Z\":" + std::to_string(z) + "}\n";
+        }
+
+        notification(str.c_str());
+        return 0;
     }
-
-    // Compute convex hull of element
-    Surface_mesh element_mesh;
-    CGAL::convex_hull_3(element_points.begin(), element_points.end(), element_mesh);
-    if (element_mesh.is_empty() && !element_mesh.is_valid())
-        return 1;
-    // Compute the extreme points of the mesh, and then a tightly fitted oriented bounding box
-    const int32_t boxSize = 8;
-    std::array<Point3D, boxSize> obb_points;
-    CGAL::oriented_bounding_box(element_mesh, obb_points, CGAL::parameters::use_convex_hull(true));
-
-    std::string str = "";
-    
-    for (int32_t i = 0; i < boxSize; i++)
+    catch (const std::exception& ex)
     {
-        auto x = obb_points[i].x();
-        auto y = obb_points[i].y();
-        auto z = obb_points[i].z();
-        auto length = std::sqrt(x * x + y * y + z * z);
-        str += "{\"Length\":" + std::to_string(length) +
-            ",\"X\":" + std::to_string(x) +
-            ",\"Y\":" + std::to_string(y) +
-            ",\"Z\":" + std::to_string(z) + "}\n";
+        auto wt = ex.what();
     }
-
-    notification(str.c_str());
-    return 0;
 }
