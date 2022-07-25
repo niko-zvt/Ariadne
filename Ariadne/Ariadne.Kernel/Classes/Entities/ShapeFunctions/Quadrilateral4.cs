e52f14394cc1db63122fc6abbd01b7a31cf39827
Nikolay Zhivotenko <niko.zvt@gmail.com>
2022-07-25 18:15:57 +0300

    Fixed bug

--- Ariadne/Ariadne.Kernel/Classes/Entities/ShapeFunctions/Quadrilateral4.cs
+++ Ariadne/Ariadne.Kernel/Classes/Entities/ShapeFunctions/Quadrilateral4.cs
@@ -112,7 +112,7 @@ namespace Ariadne.Kernel
             // 3. Preparing the optimizer
             var initParams = (-1, 1);
             var optimizer = new Math.Optimizers.MethodOfNelderMead(f, initParams);
-
+            
             // 4. Find optimum UV-coords
             var result = optimizer.FindMin(out var results);
             if (result == false || results.Length != SpaceDimension)
